f12c7f3498727d0cdbecae3850f1ae99d68bbdcd
Sean Cranbury <seancranbury@gmail.com>
2026-06-07 19:08:58 -0700

    Revise Contact page copy

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -45,15 +45,15 @@ export default function ContactPage() {
           }}
         >
           <p style={{ margin: 0 }}>
-            Thank you for visiting the Buster Frequency Project site.
+            Thank you for visiting The Buster Frequency Project.
           </p>
 
           <p style={{ margin: 0 }}>
-            It is an ongoing creative scenario where words, images, and music comingle.
+            This site is an ongoing work of fiction where words, images, music, and memory mingle with one another.
           </p>
 
           <p style={{ margin: 0 }}>
-            If you have questions then please reach out to me directly at{' '}
+            If you have questions, comments, or simply wish to say hello, please reach out directly at{' '}
             <a
               href="mailto:hello@seancranbury.com"
               style={{
@@ -68,19 +68,19 @@ export default function ContactPage() {
           </p>
 
           <p style={{ margin: 0 }}>
-            I appreciate your curiosity and your thoughts.
+            I appreciate your curiosity.
           </p>
 
           <p style={{ margin: 0 }}>
-            Support artists directly if you can. Buy their work, send them a message about what it means to you, and if you can create something yourself.
+            Support artists in your community directly whenever you can. Buy their work. Tell them what it meant to you. And if you&rsquo;re able, make something yourself. People love that stuff and you have it in you.
           </p>
 
           <p style={{ margin: 0 }}>
-            It&rsquo;s the best way to affect change in the world.
+            It&rsquo;s one of the few reliable ways to change the world.
           </p>
 
           <p style={{ margin: 0 }}>
-            Your voice and your art is valuable.
+            Your voice and your art are valuable.
           </p>
 
           <p style={{ margin: 0 }}>
@@ -101,6 +101,7 @@ export default function ContactPage() {
           }}
         >
           Sean Cranbury
+          <br />
           <br />
           June 7, 2026
         </p>
